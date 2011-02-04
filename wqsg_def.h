6ef8cf53adaca1131a978485da13510317374242
WQSG0518 <WQSG0518@763ef166-323f-0410-84cf-075efe1a78e8>
2011-02-04 07:56:33 +0000

    git-svn-id: svn://www.wqsg-svn.com/svn_WQSG_Lib/trunk@71 763ef166-323f-0410-84cf-075efe1a78e8

--- wqsg_def.h
+++ wqsg_def.h
@@ -15,29 +15,44 @@
 *  along with this program; if not, write to the Free Software
 *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 */
-#ifndef __Interface_WQSG_DEF_H__
-#define __Interface_WQSG_DEF_H__
+#ifndef __WQSG_DEF_H__
+#define __WQSG_DEF_H__
 
-#include "../wqsg_def.h"
+#if WIN32
+typedef signed	char		s8;
+typedef signed	short		s16;
+typedef signed	long		s32;
+typedef signed	__int64		s64;
 
-#define WQSG_BIT(x) (1<<(x))
+typedef unsigned char		u8;
+typedef unsigned short		u16;
+typedef unsigned long		u32;
+typedef unsigned __int64	u64;
 
-#define		__i__ 
+typedef unsigned int	uint;
+#elif defined(CELL_DATA_DIR)// end WIN32
+typedef int8_t		s8;
+typedef int16_t		s16;
+typedef int32_t		s32;
+typedef int64_t		s64;
 
+typedef uint8_t		u8;
+typedef uint16_t	u16;
+typedef uint32_t	u32;
+typedef uint64_t	u64;
 
-template<typename T>
-class CSingletonT
-{
-protected:
-	CSingletonT(){}
-	virtual ~CSingletonT(){}
-public:
-	static T& I()
-	{
-		static T _val;
-		return _val;
-	}
-};
+typedef int BOOL;
+// #define TRUE 1
+// #define FALSE 0
 
+typedef wchar_t WCHAR;
+typedef char TCHAR;
 
-#endif //__Interface_WQSG_DEF_H__
+#endif //end
+
+typedef s8	n8;
+typedef s16	n16;
+typedef s32	n32;
+typedef s64	n64;
+
+#endif
